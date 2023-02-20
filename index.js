485c181d36ea74b1e98df18bf931c36759e3b28b
David Olivé Farga <davidolivefarga@gmail.com>
2023-02-20 19:21:38 +0100

    Add dummy code for testing purposes

---
+++ index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("game-canvas");
+const canvasContext = canvas.getContext("2d");
+
+canvasContext.fillStyle = "green";
+canvasContext.fillRect(10, 10, 150, 100);
